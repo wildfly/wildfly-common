ad3558aa59eb4017e132a259ad658fc525609f48
David M. Lloyd <david.lloyd@redhat.com>
2015-02-20 12:54:47 -0600

    Add standard enum body

--- src/main/java/org/wildfly/common/ref/Reference.java
+++ src/main/java/org/wildfly/common/ref/Reference.java
@@ -18,6 +18,8 @@
 
 package org.wildfly.common.ref;
 
+import java.util.EnumSet;
+
 /**
  * An enhanced reference type with a type-safe attachment.
  *
@@ -83,5 +85,69 @@ public interface Reference<T, A> {
          * A {@code null} reference.
          */
         NULL,
+        ;
+
+        private static final int fullSize = values().length;
+
+        /**
+         * Determine whether the given set is fully populated (or "full"), meaning it contains all possible values.
+         *
+         * @param set the set
+         *
+         * @return {@code true} if the set is full, {@code false} otherwise
+         */
+        public static boolean isFull(final EnumSet<Type> set) {
+            return set != null && set.size() == fullSize;
+        }
+
+        /**
+         * Determine whether this instance is equal to one of the given instances.
+         *
+         * @param v1 the first instance
+         *
+         * @return {@code true} if one of the instances matches this one, {@code false} otherwise
+         */
+        public boolean in(final Type v1) {
+            return this == v1;
+        }
+
+        /**
+         * Determine whether this instance is equal to one of the given instances.
+         *
+         * @param v1 the first instance
+         * @param v2 the second instance
+         *
+         * @return {@code true} if one of the instances matches this one, {@code false} otherwise
+         */
+        public boolean in(final Type v1, final Type v2) {
+            return this == v1 || this == v2;
+        }
+
+        /**
+         * Determine whether this instance is equal to one of the given instances.
+         *
+         * @param v1 the first instance
+         * @param v2 the second instance
+         * @param v3 the third instance
+         *
+         * @return {@code true} if one of the instances matches this one, {@code false} otherwise
+         */
+        public boolean in(final Type v1, final Type v2, final Type v3) {
+            return this == v1 || this == v2 || this == v3;
+        }
+
+        /**
+         * Determine whether this instance is equal to one of the given instances.
+         *
+         * @param values the possible values
+         *
+         * @return {@code true} if one of the instances matches this one, {@code false} otherwise
+         */
+        public boolean in(final Type... values) {
+            if (values != null) for (Type value : values) {
+                if (this == value) return true;
+            }
+            return false;
+        }
     }
 }
