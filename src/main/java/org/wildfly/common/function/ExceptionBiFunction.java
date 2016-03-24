20ccbdbcb3f55ffde9506f148424b0963564ec04
David M. Lloyd <david.lloyd@redhat.com>
2016-03-24 16:50:27 -0500

    [WFCOM-1] Add exception-throwing functional interfaces

---
+++ src/main/java/org/wildfly/common/function/ExceptionBiFunction.java
@@ -0,0 +1,37 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2016 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.common.function;
+
+/**
+ * A two-argument function which can throw an exception.
+ *
+ * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
+ */
+@FunctionalInterface
+public interface ExceptionBiFunction<T, U, R, E extends Exception> {
+    /**
+     * Applies this function to the given arguments.
+     *
+     * @param t the first argument
+     * @param u the second argument
+     * @return the function result
+     * @throws E if an exception occurs
+     */
+    R apply(T t, U u) throws E;
+}
