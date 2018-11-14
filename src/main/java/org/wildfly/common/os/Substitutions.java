0b40f3bc511e6e849f71d0fc436da7d9b24fe342
David M. Lloyd <david.lloyd@redhat.com>
2018-11-14 17:59:04 -0600

    [WFCOM-45] Fix full pathname problem on GraalVM

--- src/main/java/org/wildfly/common/os/Substitutions.java
+++ src/main/java/org/wildfly/common/os/Substitutions.java
@@ -18,6 +18,8 @@
 
 package org.wildfly.common.os;
 
+import java.io.File;
+
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 import org.graalvm.nativeimage.ProcessProperties;
@@ -36,7 +38,11 @@ final class Substitutions {
        static String getProcessName() {
            String name = System.getProperty("jboss.process.name");
            if (name == null) {
-               name = ProcessProperties.getExecutableName();
+               String exeName = ProcessProperties.getExecutableName();
+               if (! exeName.isEmpty()) {
+                   int idx = exeName.lastIndexOf(File.separatorChar);
+                   name = idx == -1 ? exeName : idx == exeName.length() - 1 ? null : exeName.substring(idx + 1);
+               }
            }
            if (name == null) {
                name = "<unknown>";
