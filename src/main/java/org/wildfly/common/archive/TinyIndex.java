9fe97db395d5641dae4a068bb9eebce6b0225d1a
Emmanuel Hugonnet <emmanuel.hugonnet+gitkraken@gmail.com>
2019-06-07 09:42:45 +0200

    Fixing license headers

--- src/main/java/org/wildfly/common/archive/TinyIndex.java
+++ src/main/java/org/wildfly/common/archive/TinyIndex.java
@@ -1,3 +1,21 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2019 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wildfly.common.archive;
 
 import java.util.Arrays;
@@ -15,11 +33,13 @@ final class TinyIndex extends Index {
         this.table = array;
     }
 
+    @Override
     long get(final int index) {
         final int val = table[index];
         return val == -1 ? -1 : val & 0xffff;
     }
 
+    @Override
     void put(int index, final long offset) {
         final short[] table = this.table;
         int val = table[index];
