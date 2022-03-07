e1b679879cd021a909ac1165e29cbebc403b1d41
David M. Lloyd <david.lloyd@redhat.com>
2022-03-07 10:42:57 -0600

    Remove invalid @deprecated tag from package doc

--- src/main/java/org/wildfly/common/selector/package-info.java
+++ src/main/java/org/wildfly/common/selector/package-info.java
@@ -18,7 +18,5 @@
 
 /**
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
- *
- * @deprecated Use {@link org.wildfly.common.context.Contextual} instead.
  */
 package org.wildfly.common.selector;
