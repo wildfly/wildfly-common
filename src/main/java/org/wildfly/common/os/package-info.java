a0eb8cf9a5a8be2abbba14fa203c80b541f82006
David M. Lloyd <david.lloyd@redhat.com>
2024-01-12 11:21:46 -0600

    Consolidate `org.wildfly.common.os` with SmallRye
    Fixes #95

--- src/main/java/org/wildfly/common/os/package-info.java
+++ src/main/java/org/wildfly/common/os/package-info.java
@@ -20,5 +20,7 @@
  * Utilities related to the operating system and current process.
  *
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
+ * @deprecated Use {@link io.smallrye.common.os} instead.
  */
+@Deprecated(forRemoval = true)
 package org.wildfly.common.os;
